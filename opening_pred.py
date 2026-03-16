2892f8b3f0b25d2dc61139fc5ec5835a23d3fe20
Maarten Froma <maartenfroma@gmail.com>
2026-03-16 15:05:10 +0100

    Enhance data handling and model evaluation in opening_pred.py
    Added data retrieval and printing for train and test datasets. Updated seaborn import to sns and included model training and evaluation steps.

--- opening_pred.py
+++ opening_pred.py
@@ -4,15 +4,26 @@ import numpy as np
 from xgboost import XGBClassifier, plot_importance
 from sklearn.metrics import roc_auc_score, confusion_matrix
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns
 
+#Retrieve the data for both train and test file
 data_train = pd.read_csv("train.csv")
 data_test = pd.read_csv("test.csv")
+
+#Print top 5 records
 print(data_train.head(5))
-#print(data_test.head(5))
+print(data_test.head(5))
 
 print("-"*75)
 
+#Print info train file
+print(data_train.info())
+
+print("-"*75)
+#Print statistics train value
+print(data_train.describe())
+
+#Lists with columns of both numercial and categorical data
 col_num = data_train.select_dtypes(include=['int64','float64']).columns
 col_cat = data_train.select_dtypes(include=['object']).columns
 
@@ -33,13 +44,16 @@ y=data_train.iloc[:,-1].values
 #x data for test data
 x_test_kaggle=data_test[col_headers].values
 
+#Separate train file in train and test set
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,random_state=0)
 
 print("-"*75)
 
+#Create the model
 model=XGBClassifier(objective='binary:logistic',n_estimators=1000,learning_rate=0.05)
 model.fit(x_train, y_train)
 
+#Predict the model
 y_pred=model.predict_proba(x_test)[:,1]
 print("ROC AUC Score: ", roc_auc_score(y_test, y_pred))
 
@@ -62,7 +76,7 @@ cm = confusion_matrix(y_test, y_pred_binary)
 print("Confusion Matrix:\n", cm) 
 
 #Make a heatmap of the confusion matrix
-seaborn.heatmap(cm, annot=True, fmt='d')
+sns.heatmap(cm, annot=True, fmt='d')
 plt.xlabel('Predicted')
 plt.ylabel('Actual')
 plt.title('Confusion Matrix')
